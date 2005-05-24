c5e0f76305f967468e91cb164182bcc6eee81ead
yushiqi <yushiqi@90854682-670e-0410-88e8-a1cb8e504171>
2005-05-24 14:01:21 +0000

    added functions Create 2D/3D int/float/double matrix

--- src/common.h
+++ src/common.h
@@ -1,7 +1,21 @@
 #ifndef SIVP_COMMON_H
 #define SIVP_COMMON_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
 #define CV_INLINE static
+
+#ifndef TRUE
+    #define TRUE 1
+#endif
+#ifndef FALSE
+    #define FALSE 0
+#endif
+
+typedef int BOOL;
 
 #include <stdio.h>
 
@@ -10,5 +24,18 @@
 #include <highgui.h>
 
 #include <stack-c.h>
+
+  BOOL Create2DIntMat(int nPos, int nRow, int nCol, void * pData, int nType);
+  BOOL Create2DFloatMat(int nPos, int nRow, int nCol, float * pData);
+  BOOL Create2DDoubleMat(int nPos, int nRow, int nCol, double * pData);
+  BOOL Create3DIntMat(int nPos, int nRow, int nCol, int nCh, void * pData, int nType);
+  BOOL Create3DFloatMat(int nPos, int nRow, int nCol, int nCh, float* pData);
+  BOOL Create3DDoubleMat(int nPos, int nRow, int nCol, int nCh, double* pData);
+
+
+#ifdef __cplusplus
+}
+#endif
+
 
 #endif
