a8a2f24cdbe0fe03a01820b26de9c0044b32b56b
yushiqi <yushiqi@90854682-670e-0410-88e8-a1cb8e504171>
2005-07-21 09:14:03 +0000

    *** empty log message ***

--- src/canny.c
+++ src/canny.c
@@ -1,75 +1,88 @@
-//donner comme parrametre a scilab "path de l'image a traiter"
-//                                 "path ou on veut ecrire l'image traité"
-//                                 "seuil 1" 
-//                                 "seuil 2"   
+/************************************************/
+/*      NLPR & LIAMA                            */
+/*      Projet SIVP                             */
+/*      Scilab toolbox for a better world       */
+/*      2005                                    */
+/************************************************/
 
 #include "common.h"
 
-extern int toto_canny __PARAMS((char *ch,char *che,double *a,double *b));
-
 int int_canny(char *fname)
 {
-static int l1, m1, n1, l2, m2, n2, l3, m3, n3, l4, m4, n4;
-char *chaine1=NULL;
-char *chaine2=NULL;
-double *param1=NULL;
-double *param2=NULL;
+static int l2, m2, n2, l3, m3, n3, l4, m4, n4;
 
-// check nbre et nature param entre sortie
-Rhs=Max(0,Rhs);
+//create variable for parameters
+double * param1=NULL;
+double * param2=NULL;
+int * param3=NULL;
+
+//check the number of in/out parametres
+Rhs=Max(Lhs,Rhs);
 
 CheckRhs(4,4);
-CheckLhs(0,1);
+CheckLhs(1,1);
 
-//definie le param charactere d entre
-GetRhsVar(1, "c", &m1, &n1, &l1);
-GetRhsVar(2, "c", &m2, &n2, &l2);
+//check the nature of parametres
+GetRhsVar(2, "d", &m2, &n2, &l2);
 GetRhsVar(3, "d", &m3, &n3, &l3);
-GetRhsVar(4, "d", &m4, &n4, &l4);
+GetRhsVar(4, "i", &m4, &n4, &l4);
 
-chaine1 = cstk(l1);
-chaine2 = cstk(l2);
-param1 =  stk(l3);
-param2 =  stk(l4);
+// check if arg are scalaire
+if (m2*n2 != 1 || m3*n3 != 1 || m4*n4 != 1) 
+    {
+      sciprint("Error: arguments must be scalars\r\n");
+      return 0;
+    }
 
-toto_canny(chaine1,chaine2,param1,param2);
+//receive the data
+param1 =  stk(l2);
+param2 =  stk(l3);
+param3 =  istk(l4);
 
-/*
-LhsVar(1)=0;
-C2F(putlhsvar)();
-*/
+//load the input image for cvcanny
+IplImage* img1 = NULL ;
+img1=Mat2IplImg(1);
+
+// check if input image is correctly loaded
+if(img1==NULL)
+  {
+   sciprint("Error: can't read the input image\r\n");
+   return 0;
+  }
+
+//create the output image for cv canny 
+IplImage* img2 = NULL;
+img2=cvCreateImage(cvGetSize(img1),img1->depth,img1->nChannels);
+
+// check if the output image is correctly loaded
+if(img2==NULL)
+  {
+   sciprint("Error: can't create the output matrix\r\n");
+   return 0;
+  }
+
+  
+ if((img1->depth==IPL_DEPTH_8U) && (img1->nChannels==1))
+  {
+   //use the opencv function
+   cvCanny( img1, img2 ,param1[0],param2[0],param3[0]);
+   //transform the result of opencv canny in a matrice
+   IplImg2Mat(img2, 5);
+
+   //send the result
+   LhsVar(1)=5;
+  }
+ else
+ {
+  sciprint("Error: wrong input matrice\r\n");
+  return 0;
+ }  
+ 
+//let's free the memory
+cvReleaseImage( &img1 );
+cvReleaseImage( &img2 );
+
 return 0;
 }
 
-
-int toto_canny(char *ch , char *che ,double *a ,double *b)
-
-{
-double lowThresh=0 ;
-double highThresh=0;
-int apertureSize=3 ;
-
-lowThresh=a[0] ;
-highThresh=b[0];
-
-IplImage* img0 = NULL ;
-char name0[strlen(ch)];
-strcpy(name0,ch)      ;
-img0 = cvLoadImage( name0, -1 );
- 
-IplImage* img1 = NULL;
-img1=cvCreateImage(cvGetSize(img0),img0->depth,img0->nChannels);
-
-//petit coup de canny
-cvCanny( img0, img1 ,lowThresh,highThresh,apertureSize);
-
-//on sauve
-cvSaveImage(che,img1);
-//fwrite en binaire de data image
-
-// memory release for img0 before exiting the application 
-cvReleaseImage( &img0 );
-cvReleaseImage( &img1 );
-
-return(0); 
-}
+/* ©2005 LIAMA */
--- src/libsivp.c
+++ src/libsivp.c
@@ -1,14 +1,26 @@
 #include <mex.h> 
 static int direct_gateway(char *fname,void F(void)) { F();return 0;};
 extern Gatefunc int_test;
-extern Gatefunc int_canny;
 extern Gatefunc int_imread;
 extern Gatefunc int_imwrite;
+extern Gatefunc int_canny;
+extern Gatefunc int_convert;
+extern Gatefunc int_laplace;
+extern Gatefunc int_precornerdetec;
+extern Gatefunc int_imsmooth;
+extern Gatefunc int_sobel;
+extern Gatefunc int_threshold;
 static GenericTable Tab[]={
   {(Myinterfun)sci_gateway,int_test,"sivptest"},
-  {(Myinterfun)sci_gateway,int_canny,"canny"},
   {(Myinterfun)sci_gateway,int_imread,"imread"},
   {(Myinterfun)sci_gateway,int_imwrite,"imwrite"},
+  {(Myinterfun)sci_gateway,int_canny,"canny"},
+  {(Myinterfun)sci_gateway,int_convert,"convert"},
+  {(Myinterfun)sci_gateway,int_laplace,"laplace"},
+  {(Myinterfun)sci_gateway,int_precornerdetec,"precornerdetec"},
+  {(Myinterfun)sci_gateway,int_imsmooth,"imsmooth"},
+  {(Myinterfun)sci_gateway,int_sobel,"sobel"},
+  {(Myinterfun)sci_gateway,int_threshold,"threshold"},
 };
  
 int C2F(libsivp)()
