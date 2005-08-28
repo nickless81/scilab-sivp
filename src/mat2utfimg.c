7cff050c36ed708f0d9b64185aa6c0f1978a01cc
yushiqi <yushiqi@90854682-670e-0410-88e8-a1cb8e504171>
2005-08-28 08:43:07 +0000

    add video reading

---
+++ src/mat2utfimg.c
@@ -0,0 +1,79 @@
+#include "common.h"
+/* convert data from columnwise to row-wise
+ * intput: an image (M x N or M x N x 3) which must be uint8 type.
+ * output: the utf-8 image data
+ * if failed, output is 0.
+ * this function is for showing a image in TK
+ ************************************************************/
+
+int int_mat2utfimg(char * fname)
+{
+  IplImage * pImg = NULL;
+  int nOne=1, lL;
+
+  unsigned char * pUTFData = NULL;
+  int nCurr = 0;
+  BOOL isOK;
+
+  CheckRhs(1, 1);
+  CheckLhs(1, 1);
+
+  /*convert the matrix to IplImage*/
+  pImg = Mat2IplImg(1);
+  if(pImg == NULL)
+    goto EXIT_TAG;
+
+  
+  pUTFData=(unsigned char*)malloc(pImg->width * pImg->height * pImg->nChannels * 2);
+
+  int w,h,c;
+  unsigned char Pixel;
+  for(h=0; h < pImg->height; h++)
+    for(w=0; w < pImg->width; w++)
+      for(c=pImg->nChannels-1; c>=0; c--)
+	{
+	  Pixel = CV_IMAGE_ELEM(pImg, unsigned char, h, w*pImg->nChannels + c );
+	  if(Pixel <= 127)
+	    {
+	      pUTFData[nCurr++] = Pixel;
+	    }
+	  else
+	    {
+	      pUTFData[nCurr++] = (Pixel >> 6) + 0xC0; //11000000 + highest 2 bits
+	      pUTFData[nCurr++] = (Pixel & 0x3F) + 0x80;//10000000 + lest 6 bits
+	    }
+	}
+
+  pUTFData[nCurr++] = '\0';
+
+  isOK = FALSE;
+  isOK = Create2DIntMat(2, 1, nCurr, pUTFData, I_UCHAR);
+
+  if(!isOK)
+    {
+      goto EXIT_TAG;
+    }
+
+  if(pUTFData)
+    free(pUTFData);
+  if(pImg)
+    cvReleaseImage(&pImg);
+
+  LhsVar(1) = 2;
+  return 0;
+
+      //  *stk(lL) = 0;
+ EXIT_TAG:
+  if(pUTFData)
+    free(pUTFData);
+  if(pImg)
+    cvReleaseImage(&pImg);
+  /*create return value*/
+  CreateVar(2, "d", &nOne, &nOne, &lL);
+  *stk(lL) = 0;
+  LhsVar(1) = 2;
+
+
+  return 0;
+}
+
