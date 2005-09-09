2158739b6be1f08d3f9cca7a60d7067ecc12d88c
yushiqi <yushiqi@90854682-670e-0410-88e8-a1cb8e504171>
2005-09-09 07:04:00 +0000

    convert French man pages to UTF-8 encoding add GPL copyright comments to each source file

--- src/mat2utfimg.c
+++ src/mat2utfimg.c
@@ -1,3 +1,22 @@
+/***********************************************************************
+ * SIVP - Scilab Image and Video Processing toolbox
+ * Copyright (C) 2005  Vincent Etienne
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ***********************************************************************/
+
 #include "common.h"
 /* convert data from columnwise to row-wise
  * intput: an image (M x N or M x N x 3) which must be uint8 type.
