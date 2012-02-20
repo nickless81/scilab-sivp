e8498b83ba0ce1005c06988dbba1888413c8dd32
allancornet <allancornet@90854682-670e-0410-88e8-a1cb8e504171>
2012-02-20 15:09:24 +0000

    use api_scilab in sivp_init builtin

--- sci_gateway/cpp/sivp_init.cpp
+++ sci_gateway/cpp/sivp_init.cpp
@@ -1,6 +1,7 @@
 /***********************************************************************
  * SIVP - Scilab Image and Video Processing toolbox
  * Copyright (C) 2005  Vincent Etienne
+ * Copyright (C) 2012 - DIGITEO - Allan CORNET
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,27 +17,65 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  ***********************************************************************/
-
-
+/*--------------------------------------------------------------------------*/
+#include "api_scilab.h"
+#include "localization.h"
+#include "Scierror.h"
 #include "common.h"
 #include "gw_sivp.h"
-
+/*--------------------------------------------------------------------------*/
 char sSIVP_PATH[MAX_FILENAME_LENGTH];
-
+/*--------------------------------------------------------------------------*/
 /***************************************************
  * this function should be called when SIVP is loaded
  ***************************************************/
 int int_sivp_init(char * fname)
 {
+  SciErr sciErr;
+  int *piAddressVarOne = NULL;
+  char *pStr = NULL;
 
-  int mR, nR, lR;
+  CheckRhs(1, 1);
+  CheckLhs(0, 1);
 
-  CheckRhs(1,1);
-  CheckLhs(0,1);
+  sciErr = getVarAddressFromPosition(pvApiCtx, 1, &piAddressVarOne);
+  if(sciErr.iErr)
+  {
+    printError(&sciErr, 0);
+    Scierror(999, _("%s: Can not read input argument #%d.\n"), fname, 1);
+    return 0;
+  }
+
+  if (!isStringType(pvApiCtx, piAddressVarOne))
+  {
+    Scierror(999,_("%s: Wrong type for input argument #%d: A matrix of string expected.\n"), fname, 1);
+    return 0;
+  }
+
+  if (!isScalar(pvApiCtx, piAddressVarOne))
+  {
+    Scierror(999,_("%s: Wrong size for input argument #%d.\n"), fname, 1);
+    return 0;
+  }
 
   //get the directory where SIVP is intalled
-  GetRhsVar(1, "c", &mR, &nR, &lR);
-  strncpy(sSIVP_PATH, cstk(lR), MAX_FILENAME_LENGTH);
+  if (getAllocatedSingleString(pvApiCtx, piAddressVarOne, &pStr) != 0)
+  {
+    Scierror(999, _("%s: No more memory.\n"), fname);
+    return 0;
+  }
+
+  if (pStr)
+  {
+    size_t len = strlen(pStr);
+    strncpy(sSIVP_PATH, pStr, MAX_FILENAME_LENGTH);
+    if (len > 0)
+    {
+      sSIVP_PATH[strlen(pStr)] = 0;
+    }
+    freeAllocatedSingleString(pStr);
+    pStr = NULL;
+  }
 
   cvSetErrMode(CV_ErrModeParent);
   memset(OpenedAviCap, 0, sizeof(OpenedAvifileCap));
@@ -44,3 +83,5 @@ int int_sivp_init(char * fname)
   return 0;
 
 }
+/*--------------------------------------------------------------------------*/
+
